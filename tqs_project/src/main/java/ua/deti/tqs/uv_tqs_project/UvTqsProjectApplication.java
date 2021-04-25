3ff221e7f661ce7f72ada09956a0e8764f4beb9e
Anth0nyPereira <anthonypereira@ua.pt>
2021-04-25 17:31:24 +0100

    Initial commit

---
+++ tqs_project/src/main/java/ua/deti/tqs/uv_tqs_project/UvTqsProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.deti.tqs.uv_tqs_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UvTqsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UvTqsProjectApplication.class, args);
+    }
+
+}
---
+++ tqs_project/src/test/java/ua/deti/tqs/uv_tqs_project/UvTqsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.deti.tqs.uv_tqs_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UvTqsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
