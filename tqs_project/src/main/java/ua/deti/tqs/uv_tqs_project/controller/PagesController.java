44cef5d7378b5dff53f14f3c8233c02073fd953a
Anth0nyPereira <anthonypereira@ua.pt>
2021-04-29 21:58:17 +0100

    Made a modelandview in controller and now the data page shows data about the city that was chosen in the URL

--- tqs_project/src/main/java/ua/deti/tqs/uv_tqs_project/controller/PagesController.java
+++ tqs_project/src/main/java/ua/deti/tqs/uv_tqs_project/controller/PagesController.java
@@ -1,11 +1,19 @@
 package ua.deti.tqs.uv_tqs_project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import ua.deti.tqs.uv_tqs_project.AirQuality;
+import ua.deti.tqs.uv_tqs_project.service.AirQualityService;
 
 @Controller
 @RequestMapping("/home")
 public class PagesController {
+
+    @Autowired
+    private AirQualityService service;
 
     @RequestMapping("")
     public String Home() {
@@ -13,7 +21,11 @@ public class PagesController {
     }
 
     @RequestMapping("/data")
-    public String Data() {
-        return "data";
+    public ModelAndView Data(@RequestParam String city) {
+        ModelAndView modelAndView = new ModelAndView("data");
+        AirQuality airQuality = service.getData(city);
+        System.out.println(airQuality);
+        modelAndView.addObject("airquality", airQuality);
+        return modelAndView;
     }
 }
