2be3ce628ebc7e764f8c578995c0c008b84523a7
Anth0nyPereira <anthonypereira@ua.pt>
2021-05-07 22:48:14 +0100

    Added chromeJupiterExtension test

---
+++ tqs_project/src/test/java/ua/deti/tqs/airquality_tqs_project/AirQualityChromeJupiterExtensionTest.java
@@ -0,0 +1,28 @@
+package ua.deti.tqs.airquality_tqs_project;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith(SeleniumJupiter.class)
+public class AirQualityChromeJupiterExtensionTest {
+
+    @Test
+    void testChromeJupiterExtension(ChromeDriver driver) {
+        driver.get("http://localhost:8080/");
+        driver.manage().window().setSize(new Dimension(1920, 1020));
+        driver.findElement(By.cssSelector(".choices__inner")).click();
+        driver.findElement(By.cssSelector(".choices__input--cloned")).sendKeys("Aveiro");
+        driver.findElement(By.cssSelector(".choices__input--cloned")).sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector("svg")).click();
+        assertThat(driver.findElement(By.id("cityName")).getText(), is("Aveiro"));
+        driver.findElement(By.cssSelector(".go-back")).click();
+    }
+}
